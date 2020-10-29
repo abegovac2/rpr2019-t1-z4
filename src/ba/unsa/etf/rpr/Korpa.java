3c03c2bbbcdca023cc50cd647614209d9650063b
abegovac2 <abegovac2@etf.unsa.ba>
2020-10-29 19:31:00 +0100

    Dodane metode za klasu Korpa

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,46 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private Artikl[] korpa;
+    private Integer popunjeno;
+
+    public Korpa(){
+        korpa = new Artikl[50];
+        popunjeno = 0;
+    }
+
+    public Artikl[] getArtikli() {
+        return korpa;
+    }
+
+    public boolean dodajArtikl(Artikl dodaj) {
+        if(popunjeno < 50){
+            korpa[popunjeno] = new Artikl(dodaj.getNaziv(), dodaj.getCijena(), dodaj.getKod());
+            popunjeno = popunjeno + 1;
+            return true;
+        }
+        return false;
+    }
+
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for(int i = 0; i < popunjeno; ++i){
+            if(korpa[i].getKod().equals(kod)){
+                Artikl izbaceni = korpa[i];
+                if (popunjeno - 1 - i >= 0) System.arraycopy(korpa, i + 1, korpa, i, popunjeno - 1 - i);
+                korpa[popunjeno-1] = null;
+                popunjeno = popunjeno - 1;
+                return izbaceni;
+            }
+        }
+        return null;
+    }
+
+    public int dajUkupnuCijenuArtikala() {
+        int suma = 0;
+        for(int i = 0; i < popunjeno; ++i){
+            suma = suma + korpa[i].getCijena();
+        }
+        return suma;
+    }
 }
