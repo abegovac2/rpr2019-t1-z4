33195e4f3d9c23923847fe1c9aae6a3bc7ebc2e5
abegovac2 <abegovac2@etf.unsa.ba>
2020-10-29 19:31:42 +0100

    Dodane metode za klasu Supermarket

--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,41 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private Artikl[] market;
+    private Integer popunjeno;
+
+    public Supermarket(){
+        market = new Artikl[1000];
+        popunjeno = 0;
+    }
+
+    public Artikl[] getArtikli() {
+        return market;
+    }
+
+    public Integer getPopunjeno() {
+        return popunjeno;
+    }
+
+    public boolean dodajArtikl(Artikl unos) {
+        if(popunjeno < 1000){
+            market[popunjeno] = new Artikl(unos.getNaziv(), unos.getCijena(), unos.getKod());
+            popunjeno = popunjeno + 1;
+            return true;
+        }
+        return false;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for(int i = 0; i < popunjeno; ++i){
+            if(market[i].getKod().equals(kod)){
+                Artikl izbaceni = market[i];
+                if (popunjeno - 1 - i >= 0) System.arraycopy(market, i + 1, market, i, popunjeno - 1 - i);
+                market[popunjeno - 1] = null;
+                popunjeno = popunjeno - 1;
+                return izbaceni;
+            }
+        }
+        return null;
+    }
 }
